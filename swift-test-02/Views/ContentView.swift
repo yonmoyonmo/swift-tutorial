55be5785223cd954e68c0d73cf446f019544df68
yonmoyonmo <yonmosound@gmail.com>
2021-10-20 02:33:39 +0900

    xcode has no terminal lol

---
+++ swift-test-02/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  swift-test-02
+//
+//  Created by yonmo on 2021/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-test-02/Views/LandmarkDetail.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkDetail.swift
+//  swift-test-02
+//
+//  Created by yonmo on 2021/10/20.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height:300)
+                .ignoresSafeArea(edges: .top)
+            
+            SwiftUIView(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .foregroundColor(.primary)
+                
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}

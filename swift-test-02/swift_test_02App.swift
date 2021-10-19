9648771e4041d4d6544fde44ce44327468cc2876
yonmoyonmo <yonmosound@gmail.com>
2021-10-19 22:14:59 +0900

    Initial Commit

---
+++ swift-test-02/swift_test_02App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_test_02App.swift
+//  swift-test-02
+//
+//  Created by yonmo on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct swift_test_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
